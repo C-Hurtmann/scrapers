f5deac0cc6016544c1b4f2ce3ddd37a320959019
czagorodnyi <contantine2903@gmail.com>
2025-02-08 16:45:24 +0200

    connect db

--- main.py
+++ main.py
@@ -1,126 +1,67 @@
-import os
-import re
 import queue
 import threading
+import pandas as pd
 
-from concurrent.futures import ThreadPoolExecutor
 from collections import namedtuple
-from dataclasses import dataclass
-from pprint import pprint
-from time import sleep
-from typing import Any, Self
-from yarl import URL
-from lxml import etree
 
-from element import ElementNotFoundError
+from model import Base, ProductDTO, Product
 from page import Page
 from selector import By
-
-CATEGORIES = ['devops']
-
-THREADS_QTY = 3
-
-
-def write_content(file_name: str, content: Any):
-    logs_dir = 'logs'
-    os.makedirs(logs_dir, exist_ok=True)
-    with open(f'{logs_dir}/{file_name}.html', 'w', encoding='utf-8') as f:
-        f.write(etree.tostring(content, pretty_print=True, encoding='unicode'))
-
-
-@dataclass
-class Product:
-    category: str
-    subcategory: str
-    title: str | None = None
-    description: str | None = None
-    low_price: int | None = None
-    medium_price: int | None = None
-    high_price: int | None = None
-
-    @staticmethod
-    def __to_int(price: str | None) -> int | None:
-        if not price:
-            return None
-        return int(re.sub(r'[^\d]', '', price))
-
-    def collect_from_page(self, page: Page) -> Self:
-        try:
-            self.title = page.find(By().class_name('rt-Text rt-r-size-5 rt-r-weight-bold')).first().text
-            self.description = page.find(
-                By().class_name('rt-Flex rt-r-display-none xs:rt-r-display-flex rt-r-fd-row rt-r-gap-1').tag('p'),
-                raise_exception=False
-            ).first().text
-            self.low_price = self.__to_int(
-                page.find(By().class_name('v-fw-600 v-fs-12'), raise_exception=False).first().text
-            )
-            self.high_price = self.__to_int(
-                page.find(By().class_name('_rangeSliderLastNumber'), raise_exception=False).first().text
-            )
-            self.medium_price = self.__to_int(
-                page.find(By().class_name('v-fw-700 v-fs-24'), raise_exception=False).first().text
-            )
-        except ElementNotFoundError as e:
-            write_content('debug', page.source)
-            raise e
-        return self
-
-
-def fetch_product(from_: URL, category_name: str, subcategory_name: str) -> Product:
-    product_page = Page.get(root=from_)
-    product_dto = Product(category=category_name.capitalize(), subcategory=subcategory_name)
-    return product_dto.collect_from_page(page=product_page)
-
-
-def get_product_url_by_category(category_name: str):
-    category_page = Page.get(root='/categories/' + category_name)
-    subcategories = category_page.find(by=By().class_name('rt-Box rt-r-pb-1').tag('a')).all()
-    for category_num, subcategory in enumerate(subcategories, start=1):
-        subcategory_name = subcategory.text
-        subcategory_link = subcategory.link
-        write_content('debug', content=subcategory.html_element)
-        page_number = int(subcategory_link.query['page'])
-        print(f'{category_num=} | {subcategory_name}\n' + '=' * 100)
-        while True:
-            updated_link = subcategory_link.with_query(dict(subcategory_link.query) | {'page': page_number})
-            page_number += 1
-
-            product_list_page = Page.get(root=updated_link)
-            if not (
-                products := product_list_page.find(
-                    By().class_name('rt-Grid rt-r-gtc-1 sm:rt-r-gtc-2 rt-r-ai-start rt-r-gap-5').tag('a'),
-                    raise_exception=False
-                ).all()
-            ):
-                break
-            for product in products:
-                product_dto = fetch_product(
-                    from_=product.link, category_name=category_name, subcategory_name=subcategory_name
-                )
-                pprint(product_dto)
-        break
-    print(f'Finish with {category_name}')
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 
 ProductTask = namedtuple('ProductTask', ['from_', 'category_name', 'subcategory_name'])
+engine = create_engine("sqlite:///products.db")
+
+
+class TaskQueue(queue.Queue):
+
+    def __init__(self, threads_qty):
+        super().__init__()
+        self.threads_qty = threads_qty
+        self.start_workers()
+
+    def add_task(self, task, *args, **kwargs):
+        args = args or ()
+        kwargs = kwargs or {}
+        self.put((task, args, kwargs))
+
+    def start_workers(self):
+        for i in range(self.threads_qty):
+            t = threading.Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+    def worker(self):
+        while True:
+            item, args, kwargs = self.get()
+            item(*args, **kwargs)
+            self.task_done()
+
+    def wait_for_all_tasks(self):
+        self.join()
 
 
 class FlowControl:
-    to_read = queue.Queue()
-    to_write = queue.Queue()
 
     def __init__(self, threads_qty: int) -> None:
-        self.threads_qty = threads_qty
+        Base.metadata.create_all(engine)
+        self.to_read = TaskQueue(threads_qty=threads_qty)
+        self.to_write = TaskQueue(threads_qty=1)
+        self.session = sessionmaker(bind=engine)()
+        self.product_qty_to_check = 0
 
+        self.check_db_connection()
+        self.session.query(Product).delete()
+        self.session.commit()
 
-    @classmethod
-    def collect_category_products(cls, category_name: str):
+    def collect_category_products(self, category_name: str):
         category_page = Page.get(root='/categories/' + category_name)
         subcategories = category_page.find(by=By().class_name('rt-Box rt-r-pb-1').tag('a')).all()
         for category_num, subcategory in enumerate(subcategories, start=1):
             subcategory_name = subcategory.text
             subcategory_link = subcategory.link
-            write_content('debug', content=subcategory.html_element)
             page_number = int(subcategory_link.query['page'])
             print(f'{category_num=} | {subcategory_name}\n' + '=' * 100)
             while True:
@@ -136,45 +77,57 @@ class FlowControl:
                 ):
                     break
                 for product in products:
-                    cls.to_read.put(
+                    self.product_qty_to_check += 1
+                    self.to_read.add_task(
+                        self.fetch_product,
                         ProductTask(
                             from_=product.link, category_name=category_name, subcategory_name=subcategory_name
                         )
                     )
-                print('TO READ', cls.to_read.qsize())
+                print('TO READ', self.to_read.qsize())
 
-    @classmethod
-    def fetch_product(cls, task: ProductTask) -> None:
+    def fetch_product(self, task: ProductTask) -> None:
         product_page = Page.get(root=task.from_)
-        product_dto = Product(category=task.category_name.capitalize(), subcategory=task.subcategory_name)
+        product_dto = ProductDTO(category=task.category_name.capitalize(), subcategory=task.subcategory_name)
         product_dto.collect_from_page(page=product_page)
-        cls.to_write.put(product_dto)
-        print('TO WRITE', cls.to_write.qsize())
+        self.to_write.add_task(self.save_product, product_dto)
 
-    def start_worker(self):
-        while not self.to_read.empty():
-            task = self.to_read.get(timeout=2)
-            self.fetch_product(task=task)
-            self.to_read.task_done()
+    def save_product(self, product_dto: ProductDTO) -> None:
+        print('Perform save', product_dto.title)
+        product = Product(
+            category=product_dto.category,
+            subcategory=product_dto.subcategory,
+            title=product_dto.title,
+            description=product_dto.description,
+            low_price=product_dto.low_price,
+            medium_price=product_dto.medium_price,
+            high_price=product_dto.high_price,
+        )
+        self.session.add(product)
+        self.session.commit()
 
-    def start(self):
-        self.threads = []
-        for _ in range(self.threads_qty):
-            thread = threading.Thread(target=self.start_worker)
-            thread.start()
-            self.threads.append(thread)
+    def check_db_connection(self) -> None:
+        try:
+            self.session.execute(text("SELECT 1"))
+        except Exception as e:
+            raise f"❌ Database connection failed: {e}"
 
 
-def main(categories: list) -> None:
+def main(threads_qty: int, categories: list) -> None:
     print('Starting...')
     try:
-        flow_control = FlowControl(threads_qty=THREADS_QTY)
+        flow_control = FlowControl(threads_qty=threads_qty)
         for category in categories:
             flow_control.collect_category_products(category_name=category)
-        flow_control.start()
+        flow_control.to_read.wait_for_all_tasks()
+        flow_control.to_write.wait_for_all_tasks()
+        products = flow_control.session.query(Product).all()
+        print(pd.DataFrame([product.to_dict() for product in products]))
+        print('PRODUCT QTY TO CHECK', flow_control.product_qty_to_check)
     finally:
         print('THE END')
 
 
 if __name__ == '__main__':
-    main(categories=CATEGORIES)
+    categories_ = ['devops', 'it-infrastructure', 'data-analytics-and-management']
+    main(threads_qty=5, categories=categories_)
